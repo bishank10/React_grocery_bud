cfc50e3279420a03a4b691d411d173cf1a432342
bishank <chandbishank@gmail.com>
2020-12-17 11:45:52 -0700

    handle submit function added and list component rendered properly

--- src/App.js
+++ src/App.js
@@ -3,20 +3,43 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  const [name, setName]=useState("");
+  const [name, setName] = useState("");
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState([false]);
-  const [editId,setEditId]=useState(null)
-  const [alert,setAlert]=useState({show:false, msg:"",type:""});
+  const [editId, setEditId] = useState(null)
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
 
-  const handleSubmit =(e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello");
-  }
+if(!name) {
+  // display alerst
+} else if(name && isEditing){
 
-  return <section className="section-center">
-    <form className="groceryForm"></form>
+}else{
+  // shoW alert
+  const newItem = {id: new Date().getTime.toString(), title:}
+  
+}
+}
+
+  return (
+    <section className="section-center">
+      <form className="groceryForm" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input type="text" className="grocery" placeholder="e. g. eggs" value={name} onChange={(e) => setName(e.target.value)} />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          <button />
+      </div>
+    </form>
+    <div className='grocery-container'>
+      <List items={list}/>
+      <button className="clear-btn">clear items</button>   
+       </div>
   </section>
+  )
 }
 
 export default App
